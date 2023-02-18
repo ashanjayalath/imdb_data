006ad20c7655e1a44b8fde8412813d002144ccbb
ashanjayalath <shanjayalath225@gmail.com>
2023-02-18 19:32:17 +0530

    upload vercel

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const port=process.env.PORT || 8000;
 const express=require("express");
 const axios = require("axios");
 const cheerio = require("cheerio")
+const {json} = require("express");
 const app=express()
 const url="https://www.imdb.com/chart/top/?ref_=nv_mv_250"
 const url2="https://www.imdb.com/search/title/?genres=sci-fi&explore=title_type,genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=3396781f-d87f-4fac-8694-c56ce6f490fe&pf_rd_r=89EDCCTHF97ET0SZMV5R&pf_rd_s=center-1&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_pr1_i_2"
 const movieList = []
 const movieList2 = []
 
-
-app.get('/',(req,res)=> {
-    res.send("Welcome my first API")
+app.get('/',async (req,res)=> {
+    res.send("Welcome ashan jayalath free API")
 });
-
-app.get('/movies',(req,res)=>{
+app.get('/movies',async (req,res)=>{
     axios.get(url)
         .then((response)=>{
             const html=response.data
@@ -37,24 +36,29 @@ app.get('/movies',(req,res)=>{
     }).catch((e)=>console.log(e))
 
 })
-app.get('/sci-fi',(req,res)=>{
+app.get('/sci-fi',async (req,res)=>{
     axios.get(url2)
         .then((response)=>{
             const html=response.data
             const $=cheerio.load(html)
-            $('.lister-list>div',html).each((i,movie)=>{
+            $('.lister-list .lister-item-content',html).each((i,movie)=>{
                 const id=i
-                const title=$(movie).find('.lister-item-content h3 a').text()
-                const movie_year=$(movie).find('.lister-item-content h3 span.lister-item-year').text()
-                const average_time=$(movie).find('.lister-item-content p span.runtime').text()
-                const rating=$(movie).find('.lister-item-content div div strong').text()
-                const description=$(movie).find('.lister-item-content p.text-muted').text()
-                const vote_count=$(movie).find('.lister-item-content p.sort-num_votes-visible>span').text()
+                const title=$(movie).find(' h3 a').text()
+                const movie_year=parseInt($(movie).find(' h3 span.lister-item-year').text().slice(1,5))
+                const average_time_minute=parseFloat($(movie).find(' p span.runtime').text().slice(0,-4))
+                const rating=parseFloat($(movie).find(' div div strong').text())
+                const certificate=$(movie).find(' p.text-muted>span.certificate').text()
+                const genre =$(movie).find(' p.text-muted>span.genre').text().trim().slice(1)
+                $(movie).find(' p.text-muted>span').remove()
+                const description=$(movie).find(' p.text-muted').text().trim()
+                const vote_count=$(movie).find(' p.sort-num_votes-visible>span').text().trim().slice(6)
                 movieList2.push({
                     id,
                     title,
                     movie_year,
-                    average_time,
+                    certificate,
+                    genre,
+                    average_time_minute,
                     rating,
                     description,
                     vote_count
@@ -65,4 +69,5 @@ app.get('/sci-fi',(req,res)=>{
         }).catch((e)=>console.log(e))
 
 })
+
 app.listen(port,()=>console.log(`Server Start on PORT ${port}`))
